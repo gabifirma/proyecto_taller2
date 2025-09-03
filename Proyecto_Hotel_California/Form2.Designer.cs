17365b297fbed346353b17731030fd09ff1ce09d
gabifirma <gabfirmapaz@gmail.com>
2025-09-03 18:04:14 -0300

    primer commit

---
+++ Proyecto_Hotel_California/Form2.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Proyecto_Hotel_California
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.PTitulo = new System.Windows.Forms.Panel();
+            this.PLateral = new System.Windows.Forms.Panel();
+            this.BReservas = new System.Windows.Forms.Button();
+            this.BHabitaciones = new System.Windows.Forms.Button();
+            this.BEmpleados = new System.Windows.Forms.Button();
+            this.BClientes = new System.Windows.Forms.Button();
+            this.PContenedor = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.BInicio = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.BPagos = new System.Windows.Forms.Button();
+            this.PLateral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PTitulo
+            // 
+            this.PTitulo.BackColor = System.Drawing.SystemColors.Highlight;
+            this.PTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PTitulo.Location = new System.Drawing.Point(0, 0);
+            this.PTitulo.Name = "PTitulo";
+            this.PTitulo.Size = new System.Drawing.Size(884, 50);
+            this.PTitulo.TabIndex = 0;
+            // 
+            // PLateral
+            // 
+            this.PLateral.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PLateral.Controls.Add(this.panel6);
+            this.PLateral.Controls.Add(this.panel5);
+            this.PLateral.Controls.Add(this.BPagos);
+            this.PLateral.Controls.Add(this.panel4);
+            this.PLateral.Controls.Add(this.BInicio);
+            this.PLateral.Controls.Add(this.panel3);
+            this.PLateral.Controls.Add(this.panel2);
+            this.PLateral.Controls.Add(this.panel1);
+            this.PLateral.Controls.Add(this.BReservas);
+            this.PLateral.Controls.Add(this.BHabitaciones);
+            this.PLateral.Controls.Add(this.BEmpleados);
+            this.PLateral.Controls.Add(this.BClientes);
+            this.PLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PLateral.Location = new System.Drawing.Point(0, 50);
+            this.PLateral.Name = "PLateral";
+            this.PLateral.Size = new System.Drawing.Size(150, 511);
+            this.PLateral.TabIndex = 1;
+            // 
+            // BReservas
+            // 
+            this.BReservas.FlatAppearance.BorderSize = 0;
+            this.BReservas.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BReservas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BReservas.Image = ((System.Drawing.Image)(resources.GetObject("BReservas.Image")));
+            this.BReservas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BReservas.Location = new System.Drawing.Point(4, 260);
+            this.BReservas.Name = "BReservas";
+            this.BReservas.Size = new System.Drawing.Size(146, 40);
+            this.BReservas.TabIndex = 2;
+            this.BReservas.Text = "Reservas";
+            this.BReservas.UseVisualStyleBackColor = true;
+            // 
+            // BHabitaciones
+            // 
+            this.BHabitaciones.FlatAppearance.BorderSize = 0;
+            this.BHabitaciones.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BHabitaciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BHabitaciones.Image = ((System.Drawing.Image)(resources.GetObject("BHabitaciones.Image")));
+            this.BHabitaciones.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BHabitaciones.Location = new System.Drawing.Point(4, 214);
+            this.BHabitaciones.Name = "BHabitaciones";
+            this.BHabitaciones.Size = new System.Drawing.Size(146, 40);
+            this.BHabitaciones.TabIndex = 1;
+            this.BHabitaciones.Text = "Habitaciones";
+            this.BHabitaciones.UseVisualStyleBackColor = true;
+            // 
+            // BEmpleados
+            // 
+            this.BEmpleados.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BEmpleados.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.BEmpleados.FlatAppearance.BorderSize = 0;
+            this.BEmpleados.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BEmpleados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BEmpleados.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BEmpleados.Image = ((System.Drawing.Image)(resources.GetObject("BEmpleados.Image")));
+            this.BEmpleados.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BEmpleados.Location = new System.Drawing.Point(4, 168);
+            this.BEmpleados.Name = "BEmpleados";
+            this.BEmpleados.Size = new System.Drawing.Size(146, 40);
+            this.BEmpleados.TabIndex = 0;
+            this.BEmpleados.Text = "Empleados";
+            this.BEmpleados.UseVisualStyleBackColor = false;
+            this.BEmpleados.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BClientes
+            // 
+            this.BClientes.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BClientes.FlatAppearance.BorderSize = 0;
+            this.BClientes.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BClientes.Image = ((System.Drawing.Image)(resources.GetObject("BClientes.Image")));
+            this.BClientes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BClientes.Location = new System.Drawing.Point(4, 122);
+            this.BClientes.Name = "BClientes";
+            this.BClientes.Size = new System.Drawing.Size(146, 40);
+            this.BClientes.TabIndex = 0;
+            this.BClientes.Text = "Clientes";
+            this.BClientes.UseVisualStyleBackColor = true;
+            // 
+            // PContenedor
+            // 
+            this.PContenedor.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.PContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PContenedor.Location = new System.Drawing.Point(150, 50);
+            this.PContenedor.Name = "PContenedor";
+            this.PContenedor.Size = new System.Drawing.Size(734, 511);
+            this.PContenedor.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Location = new System.Drawing.Point(1, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 40);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel2.Location = new System.Drawing.Point(1, 168);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 40);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel3.Location = new System.Drawing.Point(1, 214);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(5, 40);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel4.Location = new System.Drawing.Point(1, 260);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(5, 40);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel5.Location = new System.Drawing.Point(1, 76);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(5, 40);
+            this.panel5.TabIndex = 3;
+            // 
+            // BInicio
+            // 
+            this.BInicio.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.BInicio.FlatAppearance.BorderSize = 0;
+            this.BInicio.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BInicio.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BInicio.Image = ((System.Drawing.Image)(resources.GetObject("BInicio.Image")));
+            this.BInicio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BInicio.Location = new System.Drawing.Point(4, 76);
+            this.BInicio.Name = "BInicio";
+            this.BInicio.Size = new System.Drawing.Size(146, 40);
+            this.BInicio.TabIndex = 2;
+            this.BInicio.Text = "Inicio";
+            this.BInicio.UseVisualStyleBackColor = false;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel6.Location = new System.Drawing.Point(1, 306);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(5, 40);
+            this.panel6.TabIndex = 3;
+            // 
+            // BPagos
+            // 
+            this.BPagos.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BPagos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.BPagos.FlatAppearance.BorderSize = 0;
+            this.BPagos.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BPagos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BPagos.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BPagos.Image = ((System.Drawing.Image)(resources.GetObject("BPagos.Image")));
+            this.BPagos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BPagos.Location = new System.Drawing.Point(4, 306);
+            this.BPagos.Name = "BPagos";
+            this.BPagos.Size = new System.Drawing.Size(146, 40);
+            this.BPagos.TabIndex = 2;
+            this.BPagos.Text = "Pagos";
+            this.BPagos.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.PContenedor);
+            this.Controls.Add(this.PLateral);
+            this.Controls.Add(this.PTitulo);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.PLateral.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PTitulo;
+        private System.Windows.Forms.Panel PLateral;
+        private System.Windows.Forms.Panel PContenedor;
+        private System.Windows.Forms.Button BReservas;
+        private System.Windows.Forms.Button BHabitaciones;
+        private System.Windows.Forms.Button BEmpleados;
+        private System.Windows.Forms.Button BClientes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button BInicio;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button BPagos;
+    }
+}
